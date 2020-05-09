cc0af3d8e15e5bbbf2f283f57c7269d0b130c667
rnbokade <rnbokade@gmail.com>
2020-05-09 20:57:51 +0530

    Create main.ino

---
+++ firmware/main.ino
@@ -0,0 +1,19 @@
+#include <fec.h>
+#include <rs8.h>
+uint8_t packet[200];
+uint8_t data[100];
+void setup()
+{
+	for(int i=4;i<68;i++)
+    {data[i]=0x01+i;}
+    rs_encode(&data[68],&data[4],64);
+    uint32_t syncword=0x1ACFFC1D;
+    memcpy(&data[0],&syncword,sizeof(uint32_t));
+    conv_encoder_1_2_7(packet,data,100);
+    rs_decode(&data[4],64);
+}
+
+void loop()
+{
+	
+}
